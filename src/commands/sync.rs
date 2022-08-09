0035b20ce57e79a5bc8a8d43ab7ec64599c46098
Andrew Danger Lyon <andrew@killtheradio.net>
2022-08-09 00:34:09 -0700

    sync: token derivation no longer returns pubkey (this is now derived via an aux helper). also, adding in the run fn implementation.

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -17,16 +17,13 @@ use std::convert::TryFrom;
 pub(crate) fn token(id: &str, blind: bool, regen: bool) -> Result<(), String> {
     let (master_key, transactions) = claim_pre_noval(id)?;
     let do_regen = if regen { Some(RevocationReason::Superseded) } else { None };
-    let (transactions, seckey, pubkey) = stamp_aux::sync::gen_token(&master_key, transactions, do_regen)
+    let (transactions, seckey) = stamp_aux::sync::gen_token(&master_key, transactions, do_regen)
         .map_err(|e| format!("Error generating sync key: {}", e))?;
+    let channel = stamp_aux::sync::shared_key_to_channel(&seckey)
+        .map_err(|e| format!("Error converting shared key to channel: {}", e))?;
     let identity = util::build_identity(&transactions)?;
     let id_str = id_str!(identity.id())?;
     db::save_identity(transactions)?;
-    // we can't pass around unencrypted keys by design, so instead we re-encrypt
-    // the key with a well-known secret key that we can use to unlock it later.
-    let pubkey_ser = pubkey.serialize()
-        .map_err(|e| format!("Error serializing channel pubkey: {}", e))?;
-    let channel = stamp_core::util::base64_encode(&pubkey_ser);
     let key_str = stamp_core::util::base64_encode(seckey.as_ref());
     if blind {
         let green = dialoguer::console::Style::new().green();
@@ -68,19 +65,19 @@ pub(crate) fn listen(token: &SyncToken, bind: Multiaddr, join: Vec<Multiaddr>) -
 pub(crate) fn run(id: Option<String>, token_maybe: Option<SyncToken>, join: Vec<Multiaddr>) -> Result<(), String> {
     stamp_aux::util::setup_tracing()
         .map_err(|e| format!("Error initializing tracing: {}", e))?;
-    let (id_str, shared_key) = match (token_maybe.as_ref(), id) {
-        (Some(SyncToken { ref identity_id, shared_key: Some(ref base64_key), ..}), _) => {
+    let (id_str, channel, shared_key) = match (token_maybe.as_ref(), id) {
+        (Some(SyncToken { ref identity_id, ref channel, shared_key: Some(ref base64_key), ..}), _) => {
             let bytes = stamp_core::util::base64_decode(base64_key)
                 .map_err(|e| format!("Error decoding shared key: {}", e))?;
             let key = SecretKey::new_xchacha20poly1305_from_slice(&bytes[..])
                 .map_err(|e| format!("Error decoding shared key: {}", e))?;
-            (identity_id.clone(), key)
+            (identity_id.clone(), channel.clone(), key)
         }
         (None, Some(id)) => {
             let (master_key, transactions) = claim_pre_noval(&id)?;
             let identity = util::build_identity(&transactions)?;
             let id_str = id_str!(identity.id())?;
-            let key = identity.keychain()
+            let seckey = identity.keychain()
                 .subkey_by_name("stamp/sync")
                 .map(|x| x.key().as_secretkey().clone())
                 .flatten()
@@ -88,11 +85,17 @@ pub(crate) fn run(id: Option<String>, token_maybe: Option<SyncToken>, join: Vec<
                 .transpose()
                 .map_err(|e| format!("Error opening sync key: {}", e))?
                 .ok_or(format!("Missing stamp/sync subkey for identity {} (try using a full sync token instead)", id_str))?;
-            (id_str, key)
+            let channel = stamp_aux::sync::shared_key_to_channel(&seckey)
+                .map_err(|e| format!("Error converting shared key to channel: {}", e))?;
+            (id_str, channel, seckey)
         }
         _ => Err(format!("Error selecting identity"))?,
     };
-    println!("id/key: {:?} / {:?}", id_str, shared_key);
+    println!("Syncing identity transactions...");
+    let (sent, recv) = stamp_aux::sync::run(&id_str, &channel, shared_key, join)
+        .map_err(|e| format!("Problem running sync: {}", e))?;
+    let green = dialoguer::console::Style::new().green();
+    println!("Sync finished: sent {} transactions, received {} transactions", green.apply_to(sent), green.apply_to(recv));
     Ok(())
 }
 
